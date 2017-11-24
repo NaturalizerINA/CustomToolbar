4d4f8873e849eb24232b7ee53be9aec1929275d6
NaturalizerINA <naturalizer.ina@gmail.com>
2017-11-24 16:49:41 +0700

    Add Source

---
+++ app/src/main/java/mukminullah/com/customtoolbar/MainActivity.java
@@ -0,0 +1,39 @@
+package mukminullah.com.customtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        // Hiding the title in toolbar
+        getSupportActionBar().setTitle(null);
+
+        TextView tvSave = (TextView) findViewById(R.id.toolbar_save);
+        tvSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Save Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+}
